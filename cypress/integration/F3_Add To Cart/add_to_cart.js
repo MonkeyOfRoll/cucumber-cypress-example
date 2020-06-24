170419dbebbecba62a1b0ee012ff0e20cb018427
Thomas Bowden <bowdent@ebrd.com>
2020-06-24 07:33:46 +0100

    [TB] cleaned up some non-working and unused code, logs plus fixed add cheapest/most expensive tests to do exactly that

--- cypress/integration/F3_Add To Cart/add_to_cart.js
+++ cypress/integration/F3_Add To Cart/add_to_cart.js
@@ -27,11 +27,11 @@ Then(/^The Cart badge shows the count of \"(.*?)\" item in the cart$/, (basketCo
 
 Then(/^The item is visible in the cart$/, dataTable => {
     for (const {PRODUCT_POSITION:position , PRODUCT_NAME: productName, PRODUCT_PRICE:price} of dataTable.hashes()) {
-        const shoppingListItemIndex = parseInt(position)-3
-        cy.log ('maths result: '+ shoppingListItemIndex)
+
+
         cy.get('.shopping_cart_link').click()
 
-        cy.get('#item_' + shoppingListItemIndex + '_title_link > .inventory_item_name').should('have.text',productName)
+        cy.get('.cart_list > :nth-child('+ position +') > .cart_item_label').find('.inventory_item_name').should('have.text',productName)
         cy.get('.cart_list > :nth-child('+ position +') > .cart_item_label > .item_pricebar > .inventory_item_price').should('have.text',price)
     }
 
